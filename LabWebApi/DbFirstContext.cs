d1c1bcba14ba151ed87eea1a36347c13e056fa48
YuChia <h42279@gmail.com>
2021-06-15 23:08:53 +0800

    使用命令產生 DbContext 與 Table Entity

---
+++ LabWebApi/DbFirstContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace LabWebApi
+{
+    public partial class DbFirstContext : DbContext
+    {
+        public DbFirstContext()
+        {
+        }
+
+        public DbFirstContext(DbContextOptions<DbFirstContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<DbFirstTable> DbFirstTables { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=EFCoreSample;Persist Security Info=False;User ID=SA;Password=<YourStrong@Passw0rd>;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<DbFirstTable>(entity =>
+            {
+                entity.HasKey(e => e.Id)
+                    .HasName("DbFirstTable_pk")
+                    .IsClustered(false);
+
+                entity.ToTable("DbFirstTable", "DbFirstSample");
+
+                entity.Property(e => e.Id).HasComment("?? PK ??");
+
+                entity.Property(e => e.AmountField)
+                    .HasColumnType("decimal(18, 0)")
+                    .HasComment("?????? (?????? decimal)");
+
+                entity.Property(e => e.DateTimeField).HasComment("??????");
+
+                entity.Property(e => e.StringField)
+                    .HasMaxLength(1)
+                    .HasComment("??????");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ LabWebApi/DbFirstTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace LabWebApi
+{
+    public partial class DbFirstTable
+    {
+        public int Id { get; set; }
+        public DateTime? DateTimeField { get; set; }
+        public decimal? AmountField { get; set; }
+        public string StringField { get; set; }
+    }
+}
