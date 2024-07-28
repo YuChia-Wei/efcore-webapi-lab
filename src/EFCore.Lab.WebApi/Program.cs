bc05fda6d464efe9618feec032bc159864538247
YuChia <h42279@gmail.com>
2024-07-28 22:48:40 +0800

    use top level statements program.cs

--- src/EFCore.Lab.WebApi/Program.cs
+++ src/EFCore.Lab.WebApi/Program.cs
@@ -1,18 +1,91 @@
-using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mime;
+using System.Reflection;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using EFCore.Lab.Repository;
+using EFCore.Lab.WebApi.Infrastructure.CustomJsonConverter;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
-namespace EFCore.Lab.WebApi;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+// 處理中文轉碼
+builder.Services.AddSingleton(HtmlEncoder.Create(UnicodeRanges.BasicLatin,
+                                                 UnicodeRanges.CjkUnifiedIdeographs));
+
+// API Url Path 使用小寫
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+builder.Services.AddControllers(options =>
 {
-    public static IHostBuilder CreateHostBuilder(string[] args)
-    {
-        return Host.CreateDefaultBuilder(args)
-                   .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-    }
+    // 只輸出 Json, 移除輸出 XML
+    options.OutputFormatters.RemoveType<XmlDataContractSerializerOutputFormatter>();
+}).AddJsonOptions(options =>
+{
+    // ViewModel 與 Parameter 顯示為小駝峰命名
+    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
 
-    public static void Main(string[] args)
+    // 前後端配合，輸出、入統一使用 UTC 時間
+    // 設定參考 https://github.com/dotnet/runtime/issues/1566
+    options.JsonSerializerOptions.Converters.Add(new UtcDateTimeConverter());
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = $"{AppDomain.CurrentDomain.FriendlyName} V1", Version = "v1" });
+
+    // Set the comments path for the Swagger JSON and UI.
+    var basePath = AppContext.BaseDirectory;
+    var xmlFiles = Directory.EnumerateFiles(basePath, "*.xml", SearchOption.TopDirectoryOnly);
+
+    foreach (var xmlFile in xmlFiles)
     {
-        CreateHostBuilder(args).Build().Run();
+        options.IncludeXmlComments(xmlFile);
     }
+});
+
+builder.Services.AddDbContext<EfCoreSampleContext>();
+
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+// 純 Web Api 專案不建議使用此設定
+// via https://docs.microsoft.com/zh-tw/aspnet/core/security/enforcing-ssl?view=aspnetcore-6.0&tabs=visual-studio
+// app.UseHttpsRedirection();
+
+app.UseHealthChecks("/health");
+
+app.UseSwagger()
+   .UseSwaggerUI(options =>
+   {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Lab Web Api v1");
+   });
+
+app.UseRouting();
+
+app.MapDefaultControllerRoute();
+
+app.MapControllers();
+
+app.Run();
